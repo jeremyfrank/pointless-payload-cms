81131e680af57787b9eeb434bab1827db4abbb15
Jeremy Frank <jeremyjfrank@gmail.com>
2024-04-19 08:48:21 -0400

    Add TODO placeholder for custom color field

---
+++ src/fields/colorField/index.ts
@@ -0,0 +1,4 @@
+// TODO
+// Article: https://payloadcms.com/blog/building-a-custom-field
+// Demo Fields: https://github.com/payloadcms/public-demo/tree/master/src/payload/fields
+// Real Fields: https://github.com/payloadcms/website-cms/tree/main/src/fields
